d0d0a804f097c2741975ace716888146d8f51ef8
ysx <2093811087@qq.com>
2023-08-07 16:07:25 +0800

    zht第一次提交

---
+++ zht/src/main/java/com/yc/Student.java
@@ -0,0 +1,15 @@
+package com.yc;
+
+/*
+ *@title Student
+ *@description
+ *@author zhouhaitao
+ *@version 1.0
+ *@create 2023/8/7 16:06
+ */
+public class Student {
+
+    public static void main(String[] args) {
+        System.out.println("hello student......");
+    }
+}
